11ca9411e4c567c1c9fe142f2a524d3eaa27bba6
Anastasia <mormilanastasia5@gmail.com>
2025-10-23 13:30:29 +0200

    Add project structure and code

---
+++ src/task-7.ts
@@ -0,0 +1,11 @@
+type Message = string;
+
+function getMessage(): Promise<Message> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then((result) => console.log(result));
